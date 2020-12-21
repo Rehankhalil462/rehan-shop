4279acd067954dca246de1499b6201adcbeefc99
Rehan Khalil <rehankhalil462@gmail.com>
2020-12-22 04:46:13 +0500

    shop page is made and some styling is done . collection issue has justify content issue yet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-
+import React from 'react'
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-import {Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const Hatspage = ()=>(
-  <div>Hats page hahahahah . you got it</div>
-)
+// const Hatspage = ()=>(
+  // <div>Hats page hahahahah . you got it</div>)
+
 function App() {
   return (
     <div >
       <Route exact path='/' component={Homepage}/>
-      <Route exact path='/shop/hats' component={Hatspage}/>
+      <Route exact path='/shop' component={ShopPage}/>
     </div>
   );
 }
