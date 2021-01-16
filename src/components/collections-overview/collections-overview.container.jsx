94df477262191a93d78cb3bfb7f018f544707fd5
Rehan Khalil <rehankhalil462@gmail.com>
2021-01-16 11:49:24 +0500

    Made Collection page mobile responsive which was missed in last commit

--- src/components/collections-overview/collections-overview.container.jsx
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -1,5 +1,5 @@
 import {connect} from 'react-redux';
-import {compose} from 'redux';
+// import {compose} from 'redux';
 import {createStructuredSelector} from 'reselect';
 import {selectIsCollectionFetching} from '../../redux/shop/shop.selectors';
 import WithSpinner from '../with-spinner/with-spinner.component';
